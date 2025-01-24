d9c3abdb8eca93a3ce34b42d4338f7d9096d2c45
aadityajoshi151 <aadityajoshi1501@proton.me>
2025-01-24 18:50:21 +0530

    feat: converts title and channel names into dynamic UI

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const { version } = require('os');
 const path = require('path');
 const app = express();
 const port = 6999;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.set('view engine', 'ejs');
 
 // Route for the home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/public', 'index.html'));
+  const items = Array(20).fill().map((_, i) => ({ id: i, name: `Item ${i}` })); // Create an array of 20 items
+  res.render('index', { title: "BasicTube", version: "0.9.0" , items});
 });
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
